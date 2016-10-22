8d1eb90cd80b722e7f1a1a1ccca7f482676e858b
doitforjay <jayda94@gmail.com>
2016-10-22 19:03:57 -0400

    changed work flow

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, request, session, g, redirect, url_for, render_template, flash
+from flask.ext.PyMongo import PyMongo
 app = Flask(__name__)
+mongo = PyMongo(app)
 
 @app.route('/')
 def Home():
-	return 'Hello'
+	return "hello"
 
 if __name__=="__main__":
-	app.run()
+	app.run(DEBUG=True)
+
